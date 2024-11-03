4315e670853a2cc19c0eacac5034d0b5fd0b9533
themarcos98 <marcosdanielgg98@gmail.com>
2024-11-03 16:57:49 -0300

    Second commit

---
+++ src/main/java/waRestaurant/WaRestaurantApplication.java
@@ -0,0 +1,13 @@
+package waRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/waRestaurant/WaRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package waRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
