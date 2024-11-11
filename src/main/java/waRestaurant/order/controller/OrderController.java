f533dd1876786a3df8247f4dbc97f9c668325a25
Silvia Alejandra Barrientos Roman <sbarrientos@itc.ad>
2024-11-11 18:25:42 -0300

    Se elimina metodos comentados

--- src/main/java/waRestaurant/order/controller/OrderController.java
+++ src/main/java/waRestaurant/order/controller/OrderController.java
@@ -1,7 +1,6 @@
 package waRestaurant.order.controller;
 
 import jakarta.validation.Valid;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,13 +8,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import waRestaurant.mesa.domain.MesasDto;
 import waRestaurant.order.service.OrderService;
 
 @RestController
@@ -30,24 +27,12 @@ public class OrderController {
     return ResponseEntity.ok(orderService.getAllOrdersByMesa(number));
   }
 
-//  @GetMapping("/{number}")
-//  public ResponseEntity<Object> getMesaByNumber(@PathVariable Integer number) {
-//    return ResponseEntity.of(Optional.of(orderService.getMesaById(number)));
-//  }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public void addOrder(@RequestBody @Valid OrderInput order) {
     orderService.addOrder(order);
   }
-
-//  @PutMapping("/{id}")
-//  public ResponseEntity<MesasDto> updateOrder(@PathVariable Long mesaNumber,
-//                                             @RequestBody @Valid OrderInput order) {
-//    return ResponseEntity.ok(orderService.updateMesa(id, order));
-//  }
-
-
 
   @DeleteMapping("/{id}")
   @ResponseStatus(HttpStatus.OK)
