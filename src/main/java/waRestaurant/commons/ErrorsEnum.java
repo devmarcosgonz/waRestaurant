051d0de02e59ee0de23c0a76c31d49c8a332a060
celinaM2000 <110791191+celinaM2000@users.noreply.github.com>
2024-11-11 01:54:12 +0000

    Se agregan codigos de error para manejo de excepciones en modulo de clientes

--- src/main/java/waRestaurant/commons/ErrorsEnum.java
+++ src/main/java/waRestaurant/commons/ErrorsEnum.java
@@ -11,6 +11,11 @@ public enum ErrorsEnum {
   CATEGORY_UPDATE_ERROR("EC003", "Error while updating category", HttpStatus.INTERNAL_SERVER_ERROR),
   CATEGORY_DELETE_ERROR("EC004", "Error while deleting category", HttpStatus.INTERNAL_SERVER_ERROR),
 
+  CLIENT_NOT_FOUND("ECL01", "Error not found client", HttpStatus.NOT_FOUND),
+  CLIENT_SAVE_ERROR("ECL02", "Error while saving client", HttpStatus.INTERNAL_SERVER_ERROR),
+  CLIENT_UPDATE_ERROR("ECL03", "Error while updating client", HttpStatus.INTERNAL_SERVER_ERROR),
+  CLIENT_DELETE_ERROR("ECL04", "Error while deleting client", HttpStatus.INTERNAL_SERVER_ERROR),
+
   PRODUCT_NOT_FOUND("EP001", "Error not found product", HttpStatus.NOT_FOUND),
   PRODUCT_SAVE_ERROR("EP002", "Error while saving product", HttpStatus.INTERNAL_SERVER_ERROR),
   PRODUCT_UPDATE_ERROR("EP003", "Error while updating product", HttpStatus.INTERNAL_SERVER_ERROR),
